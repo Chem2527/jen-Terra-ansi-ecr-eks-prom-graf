056947e6588b7011ada66d2b278b155ac4c201b3
sainexturn2527 <saikrishna.kakumanu@nexturn.com>
2025-04-06 14:05:08 +0530

    ok

---
+++ terraform-aws-infrastructure/modules/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "your-s3-bucket-name"
+    key            = "terraform/state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "your-dynamodb-table-name"
+    encrypt        = true
+  }
+}
