3d7fb72544e033803c6a55e1bb847f9b51b109b4
sainexturn2527 <saikrishna.kakumanu@nexturn.com>
2025-04-06 14:07:59 +0530

    ok

--- terraform-aws-infrastructure/provider.tf
+++ terraform-aws-infrastructure/provider.tf
@@ -1,3 +1,4 @@
-provider "aws" {
-  region = var.aws_region  # You can define your region in variables.tf or use a specific region
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  default     = "us-east-1"  # Replace with your desired default region
 }
