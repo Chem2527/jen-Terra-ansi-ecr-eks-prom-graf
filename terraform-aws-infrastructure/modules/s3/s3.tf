0fd815cadcf2424ca5e50feb0b65ec49402fcc8f
K.Saikrishna <108404892+Chem2527@users.noreply.github.com>
2025-04-07 09:11:07 +0530

    Update s3.tf

--- terraform-aws-infrastructure/modules/s3/s3.tf
+++ terraform-aws-infrastructure/modules/s3/s3.tf
@@ -1,30 +1,13 @@
-provider "aws" {
-    region = var.aws_region
-}
-
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = var.s3_bucket_name
-
-    versioning {
-        enabled = true
-    }
-
-    lifecycle {
-        prevent_destroy = true
-    }
+  bucket = var.s3_bucket_name
+  lifecycle {
+    # prevent_destroy = true
+  }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-    name         = var.dynamodb_table_name
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key     = "LockID"
-
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
-
-    lifecycle {
-        prevent_destroy = true
-    }
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
