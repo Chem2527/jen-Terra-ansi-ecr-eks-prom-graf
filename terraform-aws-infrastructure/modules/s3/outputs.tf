dfbabac507230c5d046faade2b1ad4e21203bfce
K.Saikrishna <108404892+Chem2527@users.noreply.github.com>
2025-04-07 09:10:52 +0530

    Update outputs.tf

--- terraform-aws-infrastructure/modules/s3/outputs.tf
+++ terraform-aws-infrastructure/modules/s3/outputs.tf
@@ -1,19 +1,3 @@
-output "s3_bucket_name" {
-    description = "The name of the S3 bucket used for Terraform state storage"
-    value       = aws_s3_bucket.terraform_state.bucket
-}
-
-output "dynamodb_table_name" {
-    description = "The name of the DynamoDB table used for Terraform state locking"
-    value       = aws_dynamodb_table.terraform_locks.name
-}
-
-output "s3_bucket_arn" {
-    description = "The ARN of the S3 bucket used for Terraform state storage"
-    value       = aws_s3_bucket.terraform_state.arn
-}
-
-output "dynamodb_table_arn" {
-    description = "The ARN of the DynamoDB table used for Terraform state locking"
-    value       = aws_dynamodb_table.terraform_locks.arn
+output "bucket_name" {
+  value = aws_s3_bucket.terraform_state.bucket
 }
