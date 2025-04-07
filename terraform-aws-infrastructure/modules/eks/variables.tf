183fec5bef8d421440a4fe6f7d2ba9c621cad7b1
K.Saikrishna <108404892+Chem2527@users.noreply.github.com>
2025-04-07 23:05:54 +0530

    Update variables.tf

--- terraform-aws-infrastructure/modules/eks/variables.tf
+++ terraform-aws-infrastructure/modules/eks/variables.tf
@@ -1,23 +1,9 @@
-# Variables
-variable "cluster_name" {
-    description = "The name of the EKS cluster"
-    type        = string
-    default     = "example"
-}
-
-variable "eks_version" {
-    description = "The version of the EKS cluster"
-    type        = string
-    default     = "1.31"
+variable "vpc_id" {
+  description = "The ID of the VPC where the EKS cluster will be deployed"
+  type        = string
 }
 
 variable "subnet_ids" {
-    description = "List of subnet IDs for the EKS cluster"
-    type        = list(string)
-}
-
-variable "iam_role_name" {
-    description = "The name of the IAM role for the EKS cluster"
-    type        = string
-    default     = "eks-cluster-example"
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
 }
