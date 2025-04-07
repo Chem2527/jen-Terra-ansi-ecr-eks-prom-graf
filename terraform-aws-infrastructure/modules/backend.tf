91dba6aaa9d56d3080b2629ba95f993196431a3d
K.Saikrishna <108404892+Chem2527@users.noreply.github.com>
2025-04-07 09:35:49 +0530

    Update backend.tf

--- terraform-aws-infrastructure/modules/backend.tf
+++ terraform-aws-infrastructure/modules/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-unique-tf-state-bucket"  # The S3 bucket you created
-    key            = "terraform/state/terraform.tfstate"
-    region         = "eu-north-1"  # The AWS region where the resources are
-    dynamodb_table = "terraform-locks"  # The DynamoDB table for state locking
-    encrypt        = true
-  }
-}
+
